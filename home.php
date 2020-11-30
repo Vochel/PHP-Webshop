ed3264078a129d43d4ffd08d653f5db8ca63a4c5
DESKTOP-AV4081V\pauli <pauli88.ew@gmail.com>
2020-11-30 18:06:30 +0100

    styles + first layout

--- home.php
+++ home.php
@@ -3,11 +3,44 @@
 
 <head>
     <meta charset="UTF-8" />
-    <title></title>
+    <link rel="stylesheet" href="styles.css">
+    <title>Willkommen</title>
 </head>
 
 <body>
+    <div class="header">
+        <h1>Herzlich Willkommen auf dem Webshop von Bruno und Erik!</h1>
+    </div>
+
+    <div class="topnav">
+        <a href="#">Thema #1</a>
+        <a href="#">Thema #2</a>
+        <a href="#">Thema #3</a>
+    </div>
+
+    <div class="row">
+        <div class="column side">
+            <h2>Side</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit..</p>
+        </div>
+
+        <div class="column middle">
+            <h2>Main Content</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus
+                venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget
+                aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor
+                sed accumsan convallis.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus
+                venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget
+                aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor
+                sed accumsan convallis.</p>
+        </div>
+    </div>
+
 
 </body>
+<footer>
+    <p>Footer</p>
+</footer>
 
 </html>
