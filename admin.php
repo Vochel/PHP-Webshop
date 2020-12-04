38d3c8fbc954934a86ab81e1a315edd4170a3559
Invisibimlims <bruno9999@freenet.de>
2020-12-04 16:40:43 +0100

    Admin Layout bearbeit

--- admin.php
+++ admin.php
@@ -51,11 +51,17 @@ session_start();
             //Datenbank auswählen
             mysqli_select_db($connection, "webshop");
 
-            //Abfrage Text
-            $sql_kat = "select nummer from kategorie where name='" . $_POST['kategorie'] . "'";
+            //Abfrage Texte
+            //if()
+            $sql = "select nummer from kategorie where name='" . $_POST['kategorie'] . "'";
+            $sql = "delete from kategorie where name='" . $_POST['kat_name_del'] . "'";
+            $sql = "insert kategorie ('name') values" . $_POST['new_kat_name'] . "'";
+            //$sql = "update kategorie set name='".$_POST[]."'where name ='".$_POST[]."'";
+
 
             //SQL-Abfrage
-            $result = mysqli_query($connection, $sql_kat);
+
+            $result = mysqli_query($connection, $sql);
 
             //Anzahl der Datensätze ermitteln
             $num = mysqli_num_rows($result);
@@ -126,12 +132,96 @@ session_start();
             } else {
                 echo "Leider keine Kategorien gefunden";
             }
+            //Hier erfolgt die erstellung der Buttons welche zur bearbeitung der Kategorien genutzt werden
+            //Bereich für löschen
+            echo "<table>";
+            echo "<tr>";
+            echo "<td>";
+            echo "<form action='admin.php?k=1' method='post'>";
+            echo "<input  type='submit'  value='Kategorie Löschen' class='kasse' style='border: none;'><br/>";
+            echo "</form>";
+            echo "</td>";
+            echo "</tr>";
+
+            //Bereich für erstellen
+            echo "<tr>";
+            echo "<td>";
+            echo "<form action='admin.php?k=2' method='post'>";
+            echo "<input  type='submit'  value='Kategorie Erstelle'class='kasse' style='border: none;'><br/>";
+            echo "</form>";
+            echo "</td>";
+            echo "</tr>";
+
+            //Bereich für aktualisieren
+            echo "<tr>";
+            echo "<td>";
+            echo "<form action='admin.php?k=3' method='post'>";
+            echo "<input  type='submit'  value='Kategorie Aktualisieren'class='kasse' style='border: none;'><br/>";
+            echo "</form>";
+            echo "</td>";
+            echo "</tr>";
+
+            echo "</table>";
             ?>
         </div>
 
         <div class="column middle">
             <?php
-            if (!empty($products)) {
+            //formular zu Löschen einer Kategorie
+            if (isset($_GET['k']) && $_GET['k'] == 1) {
+                echo "<center>
+                    <p>Hier Können Sie Kategorien Löschen!</p></center>";
+
+                echo "</br >";
+                echo "<center>";
+                echo "Geben sie die zu löschende Kategorie an.<br/><br/>";
+
+                echo "<form action='admin.php' method='post'>";
+                echo "<input type='text' name='kat_name_del'><br/><br/>";
+                echo "<input  type='submit'  value='Kategorie Löschen' name='del_kat' class='kasse' style='border: none;'><br/>";
+
+                echo "</form>";
+                echo "</center>";
+
+
+                //Bereich für das Anlegen von neuen Kategoreien
+            } elseif (isset($_GET['k']) && $_GET['k'] == 2) {
+                echo "<center>
+                    <p>Hier Können Sie Kategorien Anlegen!</p></center>";
+
+                echo "</br >";
+                echo "<center>";
+                echo "Geben sie den Namen der neuen Kategorie an.<br/><br/>";
+
+                echo "<form action='admin.php' method='post'>";
+                echo "<input type='text' name='new_kat_name'><br/><br/>";
+                echo "<input  type='submit'  value='Kategorie erstellen' name='new_kat' class='kasse' style='border: none;'><br/>";
+                echo "</form>";
+
+                echo "</center>";
+
+
+                //Bereich für das bearbeiten eines bereits existierenden Produktes
+            } elseif (isset($_GET['k']) && $_GET['k'] == 3) {
+                echo "<center>
+                    <p>Hier Können Sie Kategorien Änderen!</p></center>";
+
+                echo "</br >";
+                echo "<center>";
+                echo "Geben sie den Namen der zu bearbeitenden Kategorie an.<br/>";
+
+                echo "<form action='admin.php' method='post'>";
+                echo "<input type='text' name='kat_name'><br/><br/><br/>";
+                echo "Geben sie den neuen Namen der Kategorie an.<br/><br/>";
+                echo "<input type='text' name='kat_name_neu'><br/><br/>";
+                echo "<input  type='submit'  value='Kategorie Ändern' name='change_kat' class='kasse' style='border: none;'><br/>";
+                echo "</form>";
+
+                echo "</center>";
+
+
+                //Sollte ein Produkt geändert weden erfolgt dies im Nachfolgenden Bereich
+            } else if (!empty($products)) {
                 echo "<h2><u>" . $_POST['kategorie'] . "</u></h2>";
                 echo "<form action='home.php?e=1' method='post'>";
                 echo "<table border='1'> <tr class='table_head'><td>Produkt-Nr</td><td>Name</td><td>Preis pro Kiste</td><td>Kategorie</td><td>Herkunft</td><td>Ablaufdatum</td></tr>";
@@ -146,8 +236,31 @@ session_start();
                 }
 
                 echo "</table><br>";
-                echo "<input type='submit' value='In den Warenkorb'>";
+
+                //Hier werden die Buttons angelegt welche die Bearbeitung der Produkte Ermöglichen
+                echo "<table>";
+
+                echo "<tr>";
+                echo "<td>";
+                echo "<form action='admin.php?p=1' method='post'>";
+                echo "<input  type='submit'  value='Produkt Löschen'><br/>";
                 echo "</form>";
+                echo "</td>";
+
+                echo "<td>";
+                echo "<form action='admin.php?p=2' method='post'>";
+                echo "<input  type='submit'  value='Produkt Bearbeiten'><br/>";
+                echo "</form>";
+                echo "</td>";
+
+                echo "<td>";
+                echo "<form action='admin.php?p=3' method='post'>";
+                echo "<input  type='submit'  value='Produkt Anlegen'><br/>";
+                echo "</form>";
+                echo "</td>";
+                echo "</tr>";
+
+                echo "</table>";
             } else {
                 echo "<center><span class='material-icons'>
                         error_outline
@@ -166,7 +279,7 @@ session_start();
 
 </body>
 <footer>
-    <p>Copyright &copy; 2020 Brerik Webshops. All Rights Reserved</p>
+    <p>Copyright &copy; 2020 Brero Webshops. All Rights Reserved</p>
 </footer>
 
 
